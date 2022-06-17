f31d28abe1c16e296b2afd909785435a9201956c
IvanCrespo <contacto.ivancrespo@gmail.com>
2022-06-17 13:14:33 -0500

    Presupuestos React

---
+++ src/componentes/Header.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react'
+
+/* function Header({titulo}) {
+
+    return (
+        <Fragment>
+            <h1>{titulo}</h1>
+        </Fragment>
+    )
+} */
+
+const Header = ({ titulo }) => {
+    // const total = 1 + 2;
+    return (
+        <Fragment>
+            <h1>{titulo}</h1>
+        </Fragment>
+    );
+}
+
+export default Header;
